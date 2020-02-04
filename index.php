1e5c074a616e2a7adc50ecf4513e91730cd0f75d
Sky Richardson <skyricho@gmail.com>
2020-02-04 16:32:08 +1100

    Update index.php

--- index.php
+++ index.php
@@ -5,13 +5,6 @@ ini_set('display_errors', 1);
 $request = $fm->newFindAllCommand('collection');
 $result = $request->execute();
 
-
-$records = $result->getRecords();
-foreach($records as $record) {
-    echo $record->getField('iD') . ':<br>';
-}
-
-// or as an array
 $collection = array();
 foreach($records as $record) {
     $collection[] = array(
@@ -20,7 +13,5 @@ foreach($records as $record) {
     );
 }
 
-
-// print array
 echo '<pre>'; print_r($collection); echo '</pre>';
 ?>
